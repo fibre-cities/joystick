a656bf4c798880f79c105f24b4a29a9a3045d82c
alankillip <alan.kilip@dentsuaegis.com>
2020-02-17 17:20:48 +0000

    try onoff again

---
+++ onoff-connect.js
@@ -0,0 +1,29 @@
+// from https://www.w3schools.com/nodejs/nodejs_raspberrypi_led_pushbutton.asp
+
+const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+const up = new Gpio(32, 'in', 'both');
+const down = new Gpio(31, 'in', 'both');
+
+const upHandler = (err, value) => {
+  if (err) {
+    console.log('err', err);
+  }
+  console.log('up', value);
+};
+
+const downHandler = (err, value) => {
+  if (err) {
+    console.log('err', err);
+  }
+  console.log('up', value);
+};
+
+up.watch(upHandler);
+down.watch(downHandler);
+
+function unexportOnClose() {
+  up.unexport();
+  down.unexport();
+};
+
+process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
