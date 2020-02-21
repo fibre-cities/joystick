dfe895ec4e0d3ae22021821d7fed93b29424af6a
alankillip <alan.kilip@dentsuaegis.com>
2020-02-21 17:19:43 +0000

    link onoff callbacks with socket io emit functions

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
-// require ('./gpio-connect');
-// require ('./keyboard-connect');
-require('./onoff-connect');
+
+
+require('dotenv').config();
+
+const client  = require('socket.io-client');
+
+const onoffConnect = require('./onoff-connect');
+
+global.__basedir = __dirname;
+
+const app = require('express')();
+const http = require('http').Server(app);
+const port = process.env.PORT || 3001;
+const host = process.env.HOST;
+const city = process.env.CITY;
+
+http.listen(port, function(){
+  console.info('joystick listening on **:' + port);
+  const nameSpaceConnectionString = `${host}${city}`;
+  console.info('nameSpaceConnectionString', nameSpaceConnectionString);
+  const clientSocket = client.connect(nameSpaceConnectionString, {
+    query: {city, role: 'joystick'},
+    transports: ['websocket'],
+    transport: 'socket'
+  });
+
+  clientSocket.on('connect', onConnect);
+
+
+  function onConnect() {
+    onoffConnect(paddleUp, paddleDown, paddleStop);
+  }
+
+  function paddleUp() {
+    console.info('Emitting moveUp');
+    clientSocket.emit('moveUp', city);
+  };
+
+  function paddleDown() {
+    console.info('Emitting moveDown');
+    clientSocket.emit('moveDown', city);
+  };
+
+  function paddleStop() {
+    console.info('Emitting stop');
+    clientSocket.emit('stop', city);
+  };
+});
+
+
+
+
--- onoff-connect.js
+++ onoff-connect.js
@@ -1,29 +1,45 @@
 // from https://www.w3schools.com/nodejs/nodejs_raspberrypi_led_pushbutton.asp
+const onoffConnect = (onUp, onDown, onStop) => {
+  const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+  const up = new Gpio(6, 'in', 'both',  {debounceTimeout: 50});
+  const down = new Gpio(12, 'in', 'both', {debounceTimeout: 50});
 
-const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-const up = new Gpio(6, 'in', 'both',  {debounceTimeout: 50});
-const down = new Gpio(12, 'in', 'both', {debounceTimeout: 50});
+  const upHandler = (err, value) => {
+    if (err) {
+      console.error('err', err);
+    }
+    console.info('onoffConnect upHandler', value);
+    if (value === 0) {
+      onUp();
+    }
+    if (value === 1) {
+      onStop();
+    }
+  };
 
-const upHandler = (err, value) => {
-  if (err) {
-    console.log('err', err);
-  }
-  console.log('up', value);
+  const downHandler = (err, value) => {
+    if (err) {
+      console.log('err', err);
+    }
+    console.info('onoffConnect downHandler', value);
+    if (value === 0) {
+      onUp();
+    }
+    if (value === 1) {
+      onStop();
+    }
+  };
+
+  up.watch(upHandler);
+  down.watch(downHandler);
+
+  function unexportOnClose() {
+    up.unexport();
+    down.unexport();
+  };
+
+  process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
 };
 
-const downHandler = (err, value) => {
-  if (err) {
-    console.log('err', err);
-  }
-  console.log('down', value);
-};
+export default onoffConnect;
 
-up.watch(upHandler);
-down.watch(downHandler);
-
-function unexportOnClose() {
-  up.unexport();
-  down.unexport();
-};
-
-process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
